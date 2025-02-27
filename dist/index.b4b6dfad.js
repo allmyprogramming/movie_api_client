4fcb87a9afa04658eff46f67a05ffb22d913dffb
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2025-02-27 16:42:24 +0100

    updated hardcode to fetch

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -18588,57 +18588,27 @@ var _movieView = require("../movie-view/movie-view");
 var _s = $RefreshSig$();
 const MainView = ()=>{
     _s();
-    const [movies, setMovies] = (0, _react.useState)([
-        {
-            id: 1,
-            title: "The Shawshank Redemption",
-            image: "https://m.media-amazon.com/images/I/71KJTxW1yML._AC_SY679_.jpg",
-            director: "Frank Darabont",
-            genre: "Drama",
-            description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
-        },
-        {
-            id: 2,
-            title: "Tenet",
-            image: "https://m.media-amazon.com/images/I/91JmduXIc+L._AC_SY679_.jpg",
-            director: "Christopher Nolan",
-            genre: "Action, Sci-Fi",
-            description: "A secret agent embarks on a dangerous, time-bending mission to prevent the onset of World War III."
-        },
-        {
-            id: 3,
-            title: "Inception",
-            image: "https://m.media-amazon.com/images/I/91X1hI3FqSL._AC_SY679_.jpg",
-            director: "Christopher Nolan",
-            genre: "Action, Adventure, Sci-Fi",
-            description: "A thief who enters the dreams of others to steal secrets from their subconscious is given the task of planting an idea into the mind of a CEO."
-        },
-        {
-            id: 4,
-            title: "Dunkirk",
-            image: "https://m.media-amazon.com/images/I/91hMDezAxRL._AC_SY679_.jpg",
-            director: "Christopher Nolan",
-            genre: "Action, Drama, History",
-            description: "Allied soldiers from Belgium, the British Empire, and France are surrounded by the German army and evacuated during a fierce battle in World War II."
-        },
-        {
-            id: 5,
-            title: "The Prestige",
-            image: "https://m.media-amazon.com/images/I/51L2H+7GzHL._AC_SY679_.jpg",
-            director: "Christopher Nolan",
-            genre: "Drama, Mystery, Sci-Fi",
-            description: "Two magicians engage in a bitter rivalry, each trying to best the other's tricks in a battle for supremacy, which leads to shocking consequences."
-        },
-        {
-            id: 6,
-            title: "The Dark Knight",
-            image: "https://m.media-amazon.com/images/I/71cDDqyoEgL._AC_SY679_.jpg",
-            director: "Christopher Nolan",
-            genre: "Action, Crime, Drama",
-            description: "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, forcing Batman to come out of retirement."
-        }
-    ]);
+    const [movies, setMovies] = (0, _react.useState)([]);
     const [selectedMovie, setSelectedMovie] = (0, _react.useState)(null);
+    (0, _react.useEffect)(()=>{
+        // Fetch movies from the API
+        fetch("https://movie-api-lvgy.onrender.com/movies").then((response)=>response.json()).then((data)=>{
+            // Map through the data and format it
+            const moviesFromApi = data.map((movie)=>{
+                return {
+                    id: movie._id,
+                    title: movie.Title,
+                    image: movie.ImageUrl,
+                    director: movie.Director,
+                    genre: movie.Genre,
+                    description: movie.Description
+                };
+            });
+            setMovies(moviesFromApi); // Set the movies state
+        }).catch((error)=>{
+            console.error("Error fetching movies:", error); // Handle errors
+        });
+    }, []); // Empty dependency array to run only once on component mount
     // Function to handle the back click
     const handleBackClick = ()=>{
         setSelectedMovie(null); // Reset selected movie to null to go back to the main view
@@ -18649,15 +18619,15 @@ const MainView = ()=>{
         onBackClick: handleBackClick
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 72,
+        lineNumber: 39,
         columnNumber: 12
     }, undefined);
-    // If no movies, show a message
+    // If no movies are fetched, show a message
     if (movies.length === 0) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         children: "The list is empty!"
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 77,
+        lineNumber: 44,
         columnNumber: 12
     }, undefined);
     // Otherwise, show the list of movie cards
@@ -18665,20 +18635,20 @@ const MainView = ()=>{
         children: movies.map((movie)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieCard.MovieCard), {
                 movie: movie,
                 onMovieClick: ()=>{
-                    setSelectedMovie(movie); // Set selected movie when clicked
+                    setSelectedMovie(movie); // Set the selected movie when clicked
                 }
             }, movie.id, false, {
                 fileName: "src/components/main-view/main-view.jsx",
-                lineNumber: 84,
+                lineNumber: 51,
                 columnNumber: 9
             }, undefined))
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 82,
+        lineNumber: 49,
         columnNumber: 5
     }, undefined);
 };
-_s(MainView, "nCInFv71zKAKauEWPjPUVs/Ir4o=");
+_s(MainView, "PO+XgOji7E32nFJj3H5UPLPJ7w4=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
