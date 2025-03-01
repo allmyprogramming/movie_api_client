84865386453074a9ee8fa44f3463feb0522e86d4
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2025-03-01 17:31:32 +0100

    properties added to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie); // Trigger the onMovieClick callback when a card is clicked
-      }}
-    >
-      {movie.title}
+    <div className="movie-card" onClick={() => onMovieClick(movie)}>
+      <div>
+        <img src={movie.image} alt={movie.title} /> {/* Keeping 'image' as requested */}
+      </div>
+      <div>
+        <h3>{movie.title}</h3>
+        <p><strong>Genre:</strong> {movie.genre || "N/A"}</p>
+        <p><strong>Director:</strong> {movie.director || "N/A"}</p>
+      </div>
     </div>
   );
+};
+
+// Define PropTypes for validation
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.string,
+    director: PropTypes.string,
+    image: PropTypes.string, // Keeping 'image' as it is
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
