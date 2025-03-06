15568bddc6eab6209487f859b86331623c40709f
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2025-03-06 23:38:49 +0100

    set movie card height

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,7 @@ import { Button, Card } from "react-bootstrap";
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
 
-    <Card>
+    <Card className="h-100">
       <Card.Img variant="top" src={movie.image} alt={movie.title}/>
       <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
