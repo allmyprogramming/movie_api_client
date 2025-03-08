6cf92d7d1c0482e85089f3d2b4bf8d99ff803d3d
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2025-03-08 15:15:48 +0100

    encodeURIComponent added

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
 
     <Card className="h-100">
-      <Card.Img variant="top" src={movie.image} alt={movie.title}/>
+      <Card.Img variant="top" src={movie.image} alt={movie.title} />
       <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Text>{movie.description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="primary">
-          View Details
-        </Button>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.description}</Card.Text>
+
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="primary">View Details
+          </Button>
+        </Link>
       </Card.Body>
-      </Card>
+    </Card>
   );
 };
 
